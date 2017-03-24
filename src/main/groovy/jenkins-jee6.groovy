f0d0b33d4094dfa12a9554a01542cdd379727a54
Alexandre Touret <alexandre.touret@sihm.fr>
2017-03-24 11:55:10 +0100

    Weblogic Sonar

--- src/main/groovy/jenkins-jee6.groovy
+++ src/main/groovy/jenkins-jee6.groovy
@@ -14,14 +14,23 @@ class WeblogicConnection {
 }
 
 /**
+ * @TODO : Faire reference a un fichier sur le serveur
  * Deploie dans weblogic
  * @param artifactName
  * @param artifactPath
  * @return
  */
 def deployInWeblogic(String artifactName, String artifactPath) {
+
+    final CONNECTION = [
+            installerPath: "/logiciels/scripts/install-application-wls12c.sh ",
+            user         : "weblogic ",
+            password     : "weblogic-ic1",
+            domain       : "INTRANETHM_IC1",
+            url          : "http://wlsintegration1:7001"
+    ]
     println "weblogic deployment ..."
-    def WLS_DEPLOYMENT_COMMAND = WeblogicConnection.CONNECTION.installerPath '-u ' + WeblogicConnection.CONNECTION.user + ' -p ' + WeblogicConnection.CONNECTION.password + ' -t ' + WeblogicConnection.CONNECTION.domain + ' -s ' + WeblogicConnection.CONNECTION.url + ' -a ' + artifactName + ' -z ' + artifactPath
+    def WLS_DEPLOYMENT_COMMAND = CONNECTION.installerPath '-u ' + CONNECTION.user + ' -p ' + CONNECTION.password + ' -t ' + CONNECTION.domain + ' -s ' + CONNECTION.url + ' -a ' + artifactName + ' -z ' + artifactPath
     def process = WLS_DEPLOYMENT_COMMAND.execute()
     process.text.eachLine { println it }
     return process.exitValue()
