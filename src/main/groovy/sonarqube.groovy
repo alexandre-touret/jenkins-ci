8090d5019996f71af79b028842bdd7b4086f870d
Alexandre Touret <alexandre.touret@free.fr>
2017-03-31 13:41:18 +0200

    Test Factorisation dans un script GROOVY

--- src/main/groovy/sonarqube.groovy
+++ src/main/groovy/sonarqube.groovy
@@ -3,7 +3,16 @@
  */
 def version = '1.0'
 
+/**
+ * Execute ma commande mvn resources:resources ainsi que la qualimetrie SONAR avec le sonar-scanner
+ * @return
+ */
 def runQuality(){
+    println("Extraction du soanr-project.properties...")
+    withEnv(["JAVA_HOME=${tool 'JDK8'}", "PATH+MAVEN=${tool 'maven-3.2'}/bin:${env.JAVA_HOME}/bin"]) {
+        sh "mvn resources:resources -P ${params.PROFIL_JDK},int"
+    }
+
     println("Running SONAR QUALITY...")
     def sonarScannerHome = tool 'sonar-scanner-2.6.1'
     def workspace = pwd()
