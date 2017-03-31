c6ba09db6d73317504684c078e92c4104b26f041
Alexandre Touret <alexandre.touret@free.fr>
2017-03-31 13:25:48 +0200

    Test Factorisation dans un script GROOVY

---
+++ src/main/groovy/sonarqube.groovy
@@ -0,0 +1,13 @@
+/**
+ * Created by TOURET-A on 31/03/2017.
+ */
+def version = '1.0'
+
+def runQuality(){
+    def sonarScannerHome = tool 'sonar-scanner-2.6.1'
+    def workspace = pwd()
+    withSonarQubeEnv {
+        sh "${sonarScannerHome}/bin/sonar-scanner -Dproject.settings=${workspace}/target/classes/sonar-project.properties  -Dsonar.java.binaries=target/classes -e "
+    }
+
+}
